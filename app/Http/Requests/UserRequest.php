28430d03a725f4142785a5933739e8151fd27b67
quocdatend <quocdatend>
2025-05-08 11:38:14 +0700

    add FormRequest

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|unique:users,name|max:255',
+            'email' => 'required|string|email|unique:users,email|max:255',
+            'password' => 'required|string|min:8|confirmed|required_with:password_confirmation|same:password_confirmation',
+            'password_confirmation' => 'min:8'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
