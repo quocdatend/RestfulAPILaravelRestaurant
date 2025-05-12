46f44be3fa5c5760d35345926d4316cf5265fcd0
quocdatend <quocdatend>
2025-05-12 11:53:57 +0700

    add object party

---
+++ app/Models/Party.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Party extends Model
+{
+    protected $table = 'parties';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
